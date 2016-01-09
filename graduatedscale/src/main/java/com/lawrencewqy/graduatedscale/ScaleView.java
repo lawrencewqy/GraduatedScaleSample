5faa22e2b738bd05c20009c4a05d9f9f4adc5453
lawrencewqy <lawrencewqy@gmail.com>
2016-01-09 21:00:40 +0800

    add viewdraghelper

--- graduatedscale/src/main/java/com/lawrencewqy/graduatedscale/ScaleView.java
+++ graduatedscale/src/main/java/com/lawrencewqy/graduatedscale/ScaleView.java
@@ -2,33 +2,40 @@ package com.lawrencewqy.graduatedscale;
 
 import android.content.Context;
 import android.content.res.ColorStateList;
+import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
+import android.support.v4.view.MotionEventCompat;
 import android.support.v4.widget.ViewDragHelper;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 
 /**
  * Created by lawrence on 16/1/5.
  */
 public class ScaleView extends ViewGroup {
 
-    public static final int DEFAULT_BOTTOMLINE_STROKE = 10;
 
-    public static final int DEFAULT_LINE_STROKE = 5;
-    public static final int DEFAULT_LINE_HEIGHT = 50;
+    public static int dm2px(int dm){
+        return (int) (Resources.getSystem().getDisplayMetrics().density * dm);
+    }
 
-    public static final int DEFAULT_CURSOR_WIDTH = 5;
-    public static final int DEFAULT_CURSOR_HEIGHT = 50;
+    public static final int DEFAULT_BOTTOMLINE_STROKE = dm2px(5);
 
-    public static final int DEFAULT_LINE_SPACE = 50;
+    public static final int DEFAULT_LINE_STROKE = dm2px(5);
+    public static final int DEFAULT_LINE_HEIGHT = dm2px(25);
+
+    public static final int DEFAULT_CURSOR_WIDTH = dm2px(10);
+    public static final int DEFAULT_CURSOR_HEIGHT = dm2px(25);
+
+    public static final int DEFAULT_LINE_SPACE = dm2px(25);
     public static final int DEFAULT_LINE_COUNT = 10;
 
     ColorStateList mCursorColorList;
@@ -53,6 +60,8 @@ public class ScaleView extends ViewGroup {
     int mCursorHeight;
 
     View mCursorView;
+
+    ViewDragHelper mViewDragHelper;
 
 
 
@@ -98,7 +107,7 @@ public class ScaleView extends ViewGroup {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        int width = (mLineStrokeWidth + mLineSpace) * mLineCount + mLineStrokeWidth + getPaddingLeft() + getPaddingRight();
+        int width = (mLineStrokeWidth + mLineSpace) * mLineCount + mLineStrokeWidth +mCursorWidth+ getPaddingLeft() + getPaddingRight();
         int height = (mLineHeight + mBottomStrokeWidth + mCursorHeight) + getPaddingTop() + getPaddingBottom();
         if(mCursorDrawable != null){
             width = width+mCursorDrawable.getIntrinsicWidth();
@@ -118,6 +127,7 @@ public class ScaleView extends ViewGroup {
     public ScaleView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         //in ViewGroup if you override onDraw , you should setWillNotDraw(false)
+        mViewDragHelper = ViewDragHelper.create(this,1.0f,mCallback);
         this.setWillNotDraw(false);
         TypedArray a = context.obtainStyledAttributes(attrs,R.styleable.SearchView,defStyleAttr,0);
         mLineColorList = a.getColorStateList(R.styleable.ScaleView_scaleLineColor);
@@ -149,6 +159,48 @@ public class ScaleView extends ViewGroup {
         a.recycle();
     }
 
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        final int action = MotionEventCompat.getActionMasked(event);
+        if (action == MotionEvent.ACTION_CANCEL || action == MotionEvent.ACTION_UP) {
+            mViewDragHelper.cancel();
+            return false;
+        }
+        return mViewDragHelper.shouldInterceptTouchEvent(event);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mViewDragHelper.processTouchEvent(event);
+        return true;
+    }
+
+    ViewDragHelper.Callback mCallback = new ViewDragHelper.Callback() {
+        @Override
+        public boolean tryCaptureView(View child, int pointerId) {
+            return child == mCursorView;
+        }
+
+        @Override
+        public int clampViewPositionHorizontal(View child, int left, int dx) {
+            Log.d("ScaleView","left = "+left);
+            final int leftBound = getPaddingLeft();
+            final int rightBound = getWidth() - mCursorView.getWidth();
+            return Math.min(Math.max(left, leftBound), rightBound);
+        }
+
+        @Override
+        public int clampViewPositionVertical(View child, int top, int dy) {
+            return getHeight()-mCursorHeight;
+        }
+
+        @Override
+        public void onViewCaptured(View capturedChild, int activePointerId) {
+            super.onViewCaptured(capturedChild, activePointerId);
+            Log.d("ScaleView","capturedChild " + capturedChild);
+        }
+    };
+
     Paint mLinePaint;
     public Paint getLinePaint() {
         if(mLinePaint == null){
@@ -174,10 +226,10 @@ public class ScaleView extends ViewGroup {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         for(int i =0;i<=mLineCount;i++){
-            int left = getPaddingLeft() + (i*(mLineSpace+mLineStrokeWidth))+mLineStrokeWidth/2;
+            int left = getPaddingLeft() + (i*(mLineSpace+mLineStrokeWidth))+mLineStrokeWidth/2+mCursorWidth/2;
             int top = getPaddingRight() + mLineHeight;
             canvas.drawLine(left, top,left,getPaddingTop(), getLinePaint());
         }
-        canvas.drawLine(getPaddingLeft(),getPaddingTop()+mLineHeight,getWidth(),getPaddingTop()+mLineHeight,getBottomPaint());
+        canvas.drawLine(getPaddingLeft()+mCursorWidth/2,getPaddingTop()+mLineHeight,getWidth()-mCursorWidth/2,getPaddingTop()+mLineHeight,getBottomPaint());
     }
 }
